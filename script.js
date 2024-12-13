e604d0d1626bc6bd7c7e580598758efe99c0e190
omotayo <179114835+1827765@users.noreply.github.com>
2024-12-13 23:21:04 +0100

    Cont. UI Refactoring
    added dom element to display user and computer choice.
    display user and computer score
    text prompt to refresh page
    
    	modified:   script.js

--- script.js
+++ script.js
@@ -1,70 +1,82 @@
-const log = console.log;
 let humanScore = 0;
 let computerScore = 0;
 
-
 const getComputerChoice = () => {
-    random= Math.random().toString().charAt(5);
+    random = Math.random().toString().charAt(5);
     if (random <= 3) {
-        return 'rock'     
-    }else if (random >= 8) {
-        return 'paper'     
-    }else return 'scissors'
+        return 'rock'
+    } else if (random >= 8) {
+        return 'paper'
+    } else return 'scissors'
 
 };
-
-
-
 const playRound = (humanChoice, computerChoice) => {
+
+
     if (humanChoice === computerChoice) {
-        console.log('It\'s a tie!');
+        const TIE = document.createElement('h3')
+        TIE.textContent = 'TIE Same choice'
+        TIE.style.backgroundColor = 'red'
+        display.appendChild(TIE);
     } else if (
         (humanChoice === 'rock' && computerChoice === 'scissors') ||
         (humanChoice === 'paper' && computerChoice === 'rock') ||
         (humanChoice === 'scissors' && computerChoice === 'paper')
     ) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        const humanWins = document.createElement('h2');
+        humanWins.textContent = (`You win! ${humanChoice} beats ${computerChoice}`);
+        display.appendChild(humanWins);
         humanScore++;
+        HmSc.textContent = `Human's Score ${humanScore}`
+        HmSc.value = humanScore
+
     } else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        const computerWins = document.createElement('h2');
+        computerWins.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+        display.appendChild(computerWins);
         computerScore++;
-    }
+        CmSc.textContent = `Computer's  Score ${computerScore}`;
+        HmSc.value = computerScore;
+
+    };
+    scoring();
 };
 
 
 const scoring = () => {
 
-    if (humanScore === 3 && computerScore < 3 ) {
-        return console.log(`${humanScore}YOU BEAT THE SYSTEM!!!`)
-    } else if (computerScore === 3 && humanScore < 3 ) {
-        return console.log(`${computerScore} vs ${humanScore}THE SYSTEM WINS`);   
-    };
-    if (humanScore > 4 || computerScore > 4 ) {
-        humanScore = 0;
-        computerScore  = 0;
+    if (humanScore === 3 && computerScore < 3) {
+        const HmFinals = document.createElement('h1');
+        HmFinals.textContent = `You Beat The ProGram
+         score ${humanScore}:${computerScore}
+         REFRESH PAGE`;
+        Main.append(HmFinals)
+    } else if (computerScore === 3 && humanScore < 3) {
+        const CmFinals = document.createElement('h1');
+        CmFinals.textContent = `${computerScore}:${humanScore}
+        THE SYSTEM WINS REFRESH PAGE`;
+        Main.append(CmFinals);
+        if (HmSc.value > 3 || CmSc > 3) {
+            humanScore = 0;
+            computerScore = 0;
+            CmSc.value = computerScore;
+            HmSc.value = humanScore;
+        };
     };
 };
-
-// const playGame = () => {
-//     while (humanScore < 3 && computerScore < 3) {
-//         const humanChoice = getHumanChoice();
-//         const computerChoice = getComputerChoice();
-//         playRound(humanChoice, computerChoice);
-//         log(humanChoice);
-//         log(computerChoice);
-//         }
-//     
-
-
-
-// };
-
-// playGame();
 
 const Main = document.querySelector('#main')
 const Rock = document.createElement('button');
 const paper = document.createElement('button');
 const scissors = document.createElement('button');
+const Scores = document.createElement('div');
+Scores.style.display = 'flex'
+const HmSc = document.createElement('h4')
+const CmSc = document.createElement('h4')
+HmSc.style.cssText = 'margin: 0 auto 0 auto;'
+Scores.appendChild(HmSc);
+Scores.appendChild(CmSc)
+Main.appendChild(Scores)
 Main.appendChild(Rock);
 Main.appendChild(paper);
 Main.appendChild(scissors);
@@ -79,22 +91,22 @@ scissors.value = 'scissors'
 
 Rock.addEventListener('click', (humanChoice, computerChoice) => {
     humanChoice = Rock.value;
-    computerChoice  = getComputerChoice()
+    computerChoice = getComputerChoice();
     playRound(humanChoice, computerChoice);
-    scoring();
 });
 paper.addEventListener('click', (humanChoice, computerChoice) => {
     humanChoice = paper.value;
-    computerChoice = getComputerChoice()
-    playRound(humanChoice,computerChoice);
-    scoring();
+    computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
 });
 
-scissors.addEventListener('click',(humanChoice, computerChoice) => {
+scissors.addEventListener('click', (humanChoice, computerChoice) => {
     humanChoice = scissors.value;
     computerChoice = getComputerChoice();
-    console
-    playRound(humanChoice, computerChoice)
-    scoring();
+    playRound(humanChoice, computerChoice);
 });
+
+const display = document.createElement('div')
+Main.appendChild(display);
+
 
